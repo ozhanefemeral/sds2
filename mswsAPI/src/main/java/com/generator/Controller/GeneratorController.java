a1d42adcf4c33cb625856a79e97284532d625131
ProOko <prooko99@gmail.com>
2024-01-11 20:12:27 +0100

    CROS policy

--- mswsAPI/src/main/java/com/generator/Controller/GeneratorController.java
+++ mswsAPI/src/main/java/com/generator/Controller/GeneratorController.java
@@ -2,11 +2,13 @@ package com.generator.Controller;
 
 import com.generator.Generator.MiddleSquareWeylSequenceRNG;
 import com.generator.Generator.RandomNumberGenerator;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class GeneratorController {
     private final RandomNumberGenerator generator = new MiddleSquareWeylSequenceRNG();
 
